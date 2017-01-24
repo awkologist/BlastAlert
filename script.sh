d6f1e179a1879d6b930029beae8ba684fe104c7e
Awkologist <rw@biowasserstoff.de>
2017-01-24 20:22:48 +0100

    exchanged links and linked file

--- script.sh
+++ script.sh
@@ -1,1 +1,10 @@
-/usr/lib/cgi-bin/script.sh
+#!/bin/bash
+echo "Content-type: text/html; charset=UTF-8"
+echo  
+echo "<html><body>"
+echo "$(date)"
+echo "<br><br>"
+echo "<pre>$(/home/awkologist/BioLinux/blastalert.sh
+)</pre>"
+cp blastalert.txt /home/awkologist/BioLinux/blastalert-www.txt
+echo "</html></body>"
--- show.sh
+++ show.sh
@@ -1,1 +1,6 @@
-/usr/lib/cgi-bin/show.sh
+#!/bin/bash
+echo "Content-type: text/html; charset=UTF-8"
+echo  
+echo "<html><body>"
+echo "<pre>$(cat /home/awkologist/BioLinux/blastalert-www.txt)</pre>"
+echo "</html></body>"
